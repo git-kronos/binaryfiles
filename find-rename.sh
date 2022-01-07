33baec6ee09baecc5c866b1c00f6044b6b2f4a00
Shuvam Das <repository.shuvam@gmail.com>
2022-01-08 00:25:27 +0530

    modification in all file

--- find-rename.sh
+++ find-rename.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# read -p "type= " type
-type=$1
-name=$2
+type="$1"
+name="$2"
+
 main() {
 	pattern=${name// /*}
 	find . -type $type -iname "*$pattern*"
 	read opt
-	if [ $opt -eq 1 ]; then
+	if [ "$opt" -eq 1 ]; then
 		mkdir "#$name"
-		find . -type $type -iname "*$pattern*" -exec mv {} "#$name" \;
+		find . -type "$type" -iname "*$pattern*" -exec mv {} "#$name" +
 	fi
 }
 
